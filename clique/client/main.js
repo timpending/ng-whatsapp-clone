43f35d8979dbe61d12e90cfbe0bdef0304747595
Tim Pender <mrtimpender@gmail.com>
2016-09-06 17:10:43 -0600

    through the helper Angular 3way databinding

--- clique/client/main.js
+++ clique/client/main.js
@@ -3,4 +3,14 @@ import angularMeteor from 'angular-meteor';
 
 angular.module('clique', [
   angularMeteor
-]);
+  ])
+  .controller('PartiesListController', function($scope) {
+    'ngInject';
+
+    $scope.helpers({
+      parties() {
+        return Parties.find({});
+      }
+    })
+
+  });
---
+++ clique/server/startup.js
@@ -0,0 +1,18 @@
+Meteor.startup(() => {
+  if (Parties.find().count() === 0) {
+    const parties = [{
+      'name': 'Pants-Party',
+      'description': 'Bricks Favorite'
+    },{
+      'name': 'Toga Party',
+      'description': 'Friends, Romans, Countrymen, lend me your bedsheets'
+    },{
+      'name': 'Birthday Party',
+      'description': 'Give me your cupcakes'
+    }];
+
+    parties.forEach((party) => {
+      Parties.insert(party)
+    });
+  }
+});
